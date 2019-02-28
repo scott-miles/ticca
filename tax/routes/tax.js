7ab136bd01fbcd3dff8341866593fc86e57637be
Scott Miles <scott.miles@myob.com>
2019-02-28 15:01:39 +1100

    workshop in progress

--- tax/routes/tax.js
+++ tax/routes/tax.js
@@ -4,24 +4,24 @@ var taxService = require('./taxService');
 var idamService = require('./idamService');
 
 router.get('/tax', (request, response, next) => {
-    // if (!request.headers['token']) {
-    //     response.status(406);
-    //     response.send({ error: 'No token' });
-    //     return;
-    // }
-    // idamService.authenticate(request.headers['token'], function(authenticated){
-    //     if(authenticated == 'false') {
-    //         response.status(401);
-    //         response.send({ error: 'Invalid token' });
-    //         return;
-    //     }
-    //     if(authenticated == 'error') {
-    //         response.status(500);
-    //         response.send({ error: 'Cannot connect to login server' });
-    //         return;
-    //     }
+    if (!request.headers['token']) {
+        response.status(406);
+        response.send({ error: 'No token' });
+        return;
+    }
+    idamService.authenticate(request.headers['token'], function(authenticated){
+        if(authenticated == 'false') {
+            response.status(401);
+            response.send({ error: 'Invalid token' });
+            return;
+        }
+        if(authenticated == 'error') {
+            response.status(500);
+            response.send({ error: 'Cannot connect to login server' });
+            return;
+        }
          taxService.getIncomeTax(request, response, next);
-    // });
+     });
 });
 
 module.exports = router;
--- tax/test/spec/taxSpec.js
+++ tax/test/spec/taxSpec.js
@@ -3,10 +3,31 @@ let mocha = require('mocha');
 let taxAPI = require('../api/taxAPI.js')
 
 describe('Calculate tax for an employee', function() {
+
   context('with a taxable income of 0', function(){
     it('reponse should have income tax of 0', function(done){
       taxAPI.getTaxForIncome(0, function(response) {
         response.body.should.have.property('incomeTax', 0);
+        response.should.have.status(200);
+        done();
+      });
+    });
+  });
+
+  context('with a taxable income of 20000', function(){
+    it('reponse should have income tax of 342', function(done){
+      taxAPI.getTaxForIncome(20000, function(response) {
+        response.body.should.have.property('incomeTax', 342);
+        response.should.have.status(200);
+        done();
+      });
+    });
+  });
+
+  context('with a taxable income of 100000', function(){
+    it('reponse should have income tax of 24947', function(done){
+      taxAPI.getTaxForIncome(100000, function(response) {
+        response.body.should.have.property('incomeTax', 24947);
         response.should.have.status(200);
         done();
       });
